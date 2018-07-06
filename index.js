74f2bc042fa3578305b52a4c40f95bdd0da61528
udaybhola3 <uday.bhola@venturepact.com>
2018-07-06 11:15:51 +0530

    Add list message

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ function decideMessage(sender, text1){
     else if(text.includes("rainy")){
         sendRainyImageMessage(sender);
         console.log("rainy season, inside decide message function");
+    }else if(text.includes("list")){
+        sendListMessage(sender);
     }
     else{
      sendText(sender, "I Like Fall");
@@ -152,6 +154,76 @@ function sendRainyImageMessage(sender){
         }
     sendRequest(sender, messageData);
 }
+
+function sendListMessage(sender){
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "list",
+              "top_element_style": "compact",
+              "elements": [
+                {
+                  "title": "Classic T-Shirt Collection",
+                  "subtitle": "See all our colors",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+                  "buttons": [
+                    {
+                      "title": "View",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                    }
+                  ]
+                },
+                {
+                  "title": "Classic White T-Shirt",
+                  "subtitle": "See all our colors",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                    "messenger_extensions": false,
+                    "webview_height_ratio": "tall"
+                  }
+                },
+                {
+                  "title": "Classic Blue T-Shirt",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                  "subtitle": "100% Cotton, 200% Comfortable",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                  },
+                  "buttons": [
+                    {
+                      "title": "Shop Now",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                    }
+                  ]        
+                }
+              ],
+               "buttons": [
+                {
+                  "title": "View More",
+                  "type": "postback",
+                  "payload": "payload"            
+                }
+              ]  
+            }
+          }
+        }
+        sendRequest(sender, messageData);
+    }
+
 
 function sendRequest(sender, messageData){
     request({
